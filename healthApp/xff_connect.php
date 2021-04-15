45059fc0b28ba2aed9480f5e1f07b299512870dc
Heather Lynn <heatherapellynn@gmail.com>
2021-04-15 08:03:33 -0400

    Delete xff_connect.php

--- healthApp/xff_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('mysql01-dc3.mycorphosting.com:3306', 'heather', '2vEt8?x2', 'commulis_');
-//$mysqli = new mysqli('localhost', 'root', '', 'commulis_');
-
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
-?>
